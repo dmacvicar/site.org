b6fca152a4698673eac751ddddef5ccc089a06a7
Duncan Mac-Vicar P <dmacvicar@gmail.com>
2019-08-07 07:31:04 +0200

    Use nicolas.petton.fr inspired configuration

--- publish.el
+++ publish.el
@@ -16,53 +16,91 @@
 (package-install 'htmlize)
 
 (require 'org)
+(require 'ox-rss)
 (require 'ox-publish)
 
-;; setting to nil, avoids "Author: x" at the bottom
-(setq user-full-name nil)
 
-(setq org-export-with-section-numbers nil
-      org-export-with-smart-quotes t
-      org-export-with-toc nil)
+(defvar duncan-website-html-head
+"<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic' rel='stylesheet' type='text/css'>
+<link rel='stylesheet' href='css/site.css' type='text/css'/>")
 
-(setq org-html-divs '((preamble "header" "top")
-                      (content "main" "content")
-                      (postamble "footer" "postamble"))
-      org-html-container-element "section"
-      org-html-metadata-timestamp-format "%Y-%m-%d"
-      org-html-checkbox-type 'html
-      org-html-html5-fancy t
-      org-html-validation-link nil
-      org-html-doctype "html5")
+(defvar duncan-website-html-blog-head
+"<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic' rel='stylesheet' type='text/css'>
+<link rel='stylesheet' href='../css/site.css' type='text/css'/>")
 
-(defvar site-attachments
-  (regexp-opt '("jpg" "jpeg" "gif" "png" "svg"
-                "ico" "cur" "css" "js" "woff" "html" "pdf"))
-  "File types that are published as static files.")
+(defvar duncan-website-html-preamble 
+  "<div class='nav'>
+<ul>
+<li><a href='/'>Home</a></li>
+<li><a href='/blog/index.html'>Blog</a></li>
+<li><a href='http://github.com/dmacvicar'>GitHub</a></li>
+<li><a href='http://twitter.com/dmacvicar'>Twitter</a></li>
+<li><a href='/contact.html'>Contact</a></li>
+</ul>
+</div>")
+
+(defvar duncan-website-html-postamble 
+  "<div class='footer'>
+Copyright 2019 %a (%v HTML).<br>
+Last updated %C. <br>
+Built with %c.
+</div>")
 
 (setq org-publish-project-alist
-      (list
-       (list "site-org"
-             :base-directory "."
-             :base-extension "org"
-             :recursive t
-             :publishing-function '(org-html-publish-to-html)
-             :publishing-directory "./public"
-             :exclude (regexp-opt '("README" "draft"))
-             :auto-sitemap t
-             :sitemap-filename "index.org"
-             :sitemap-file-entry-format "%d *%t*"
-             :html-head-extra "<link rel=\"icon\" type=\"image/x-icon\" href=\"/favicon.ico\"/>"
-             :sitemap-style 'list
-             :sitemap-sort-files 'anti-chronologically)
-       (list "site-static"
-             :base-directory "."
-             :exclude "public/"
-             :base-extension site-attachments
-             :publishing-directory "./public"
-             :publishing-function 'org-publish-attachment
-             :recursive t)
-       (list "site" :components '("site-org"))))
+      `(("org"
+         :base-directory "."
+         :base-extension "org"
+         :publishing-directory "./public"
+         :publishing-function org-html-publish-to-html
+         :section-numbers nil
+         :with-toc nil
+         :html-head ,duncan-website-html-head
+         :html-preamble ,duncan-website-html-preamble
+         :html-postamble ,duncan-website-html-postamble)
+
+        ("blog"
+         :base-directory "./blog"
+         :base-extension "org"
+         :publishing-directory "./public/blog"
+         :publishing-function org-html-publish-to-html
+         :section-numbers nil
+         :with-toc nil
+         :html-head ,duncan-website-html-blog-head
+         :html-head-extra
+         "<link rel=\"alternate\" type=\"application/rss+xml\"
+                href=\"http://duncan.codes/blog/blog.xml\"
+                title=\"RSS feed\">"
+         :html-preamble ,duncan-website-html-preamble
+         :html-postamble ,duncan-website-html-postamble)
+
+        ("images"
+         :base-directory "./assets/images"
+         :base-extension "jpg\\|gif\\|png"
+         :publishing-directory "./public/images"
+         :publishing-function org-publish-attachment)
+
+        ("js"
+         :base-directory "./assets/js"
+         :base-extension "js"
+         :publishing-directory "./public/js"
+         :publishing-function org-publish-attachment)
+
+        ("css"
+         :base-directory "./assets/css"
+         :base-extension "css"
+         :publishing-directory "./public/css/"
+         :publishing-function org-publish-attachment)
+
+        ("rss"
+         :base-directory "./blog"
+         :base-extension "org"
+         :publishing-directory "./blog"
+         :publishing-function (org-rss-publish-to-rss)
+         :html-link-home "http://duncan.codes/"
+         :html-link-use-abs-url t)
+
+        ("website" :components ("org" "blog" "images" "js" "css" "rss"))))
+
 
 (provide 'publish)
 ;;; publish.el ends here
