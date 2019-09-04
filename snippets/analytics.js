81c4eda21349a0f3762a27b3c4572095df3625b4
Duncan Mac-Vicar P <dmacvicar@gmail.com>
2019-09-04 14:50:55 +0200

    Fix Google Analytics with the recommended snippet

--- snippets/analytics.js
+++ snippets/analytics.js
@@ -1,8 +1,9 @@
-<!-- Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1687066-2"></script>
 <script>
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', 'UA-1687066-2', 'auto');
-ga('send', 'pageview');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-1687066-2');
 </script>
-<script async src='https://www.google-analytics.com/analytics.js'></script>
-<!-- End Google Analytics -->
